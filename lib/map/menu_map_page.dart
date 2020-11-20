b9120611dee5e5ca4286d1d787e2a07528038fa5
Salvador CG <ie708498@iteso.mx>
2020-11-20 13:55:11 -0600

    adding a list of users and show a card for every user

--- lib/map/menu_map_page.dart
+++ lib/map/menu_map_page.dart
@@ -8,6 +8,109 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:share/share.dart';
 
+class Users {
+  final LatLng coord;
+  final String firstname;
+  final String lastname;
+
+  Users(
+    {
+      @required this.coord,
+      @required this.firstname,
+      @required this.lastname
+    }
+  );
+}
+
+List<LatLng> _coords = [
+  LatLng(20.659684585453792, -103.45644380897282),
+  LatLng(20.58794293024975,  -103.41349761933088),
+  LatLng(20.709674113035824, -103.38253621011972),
+  LatLng(20.6232340206758,   -103.32710534334183),
+  LatLng(20.703134533765034, -103.29913996160033)  
+];
+
+final List<Users> _usersList = [
+  Users(
+    coord: _coords[0],
+    firstname: 'Juan',
+    lastname: 'Perez',
+  ),
+  Users(
+    coord: _coords[1],
+    firstname: 'Juan',
+    lastname: 'Colorado',
+  ),
+  Users(
+    coord: _coords[2],
+    firstname: 'Juan',
+    lastname: 'Escutia',
+  ),
+  Users(
+    coord: _coords[3],
+    firstname: 'Juan',
+    lastname: 'Lopez',
+  ),  
+  Users(
+    coord: _coords[4],
+    firstname: 'Juan',
+    lastname: 'De la Barrera',
+  ),         
+];
+
+Set<Marker> _mapMarkers = {
+  Marker(
+    markerId: MarkerId(_coords[0].toString()),
+    position: _coords[0],
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+    infoWindow: InfoWindow(
+      title: _coords[0].toString(),
+      //snippet: _getGeolocationAddress(Position(latitude: _coords[0].latitude, longitude: _coords[0].longitude)),
+    ),
+    onTap: () {},
+  ),
+  Marker(
+    markerId: MarkerId(_coords[1].toString()),
+    position: _coords[1],
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+    infoWindow: InfoWindow(
+      title: _coords[1].toString(),
+      //snippet: _getGeolocationAddress(Position(latitude: _coords[0].latitude, longitude: _coords[0].longitude)),
+    ),
+    onTap: () {},
+  ), 
+   Marker(
+    markerId: MarkerId(_coords[2].toString()),
+    position: _coords[2],
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+    infoWindow: InfoWindow(
+      title: _coords[2].toString(),
+      //snippet: _getGeolocationAddress(Position(latitude: _coords[0].latitude, longitude: _coords[0].longitude)),
+    ),
+    onTap: () {},
+  ), 
+   Marker(
+    markerId: MarkerId(_coords[3].toString()),
+    position: _coords[3],
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+    infoWindow: InfoWindow(
+      title: _coords[3].toString(),
+      //snippet: _getGeolocationAddress(Position(latitude: _coords[0].latitude, longitude: _coords[0].longitude)),
+    ),
+    onTap: () {},
+  ),
+   Marker(
+    markerId: MarkerId(_coords[4].toString()),
+    position: _coords[4],
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+    infoWindow: InfoWindow(
+      title: _coords[4].toString(),
+      //snippet: _getGeolocationAddress(Position(latitude: _coords[0].latitude, longitude: _coords[0].longitude)),
+    ),
+    onTap: () {},
+  ),         
+};
+
 class MapPage extends StatefulWidget {
   final HomeBloc bloc;
   final BuildContext context;
@@ -24,9 +127,8 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   GoogleMapController _mapController;
-  Set<Marker> _mapMarkers = Set();
-  Marker _currentPositionMarker;
   Position _currentPosition;
+  int _user_index = 0;
   final LatLng _center = const LatLng(20.608160, -103.414496);
 
   @override
@@ -56,6 +158,14 @@ class _MapPageState extends State<MapPage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
+                  FloatingActionButton(
+                    backgroundColor: kLightPurple,
+                    child: Icon(Icons.people),
+                    onPressed: (){
+                      _showUserCard(context,_usersList[_user_index]);
+                    },
+                  ),
+                  SizedBox(height: 30),                  
                   FloatingActionButton(
                     backgroundColor: kLightPurple,
                     child: Icon(Icons.location_pin),
@@ -130,43 +240,6 @@ class _MapPageState extends State<MapPage> {
       desiredAccuracy: LocationAccuracy.high
     );
 
-    // get address
-    String _currentAddress = await _getGeolocationAddress(_currentPosition);
-
-    LatLng coord = LatLng(_currentPosition.latitude,_currentPosition.longitude);
-    // add marker
-    _mapMarkers.add(
-      Marker(
-        markerId: MarkerId(_currentPosition.toString()),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
-        position: LatLng(
-          _currentPosition.latitude,
-          _currentPosition.longitude,
-        ),
-        infoWindow: InfoWindow(
-          title: _currentPosition.toString(),
-          snippet: _currentAddress,
-        ),
-        consumeTapEvents: true,
-        onTap: () async {
-          await showModalBottomSheet(
-            context: context,
-            builder: (context) => StatefulBuilder(
-              builder: (context, setModalState) =>
-                  _bottomSheet(context,setModalState,coord,_currentAddress),
-            ),
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15.0),
-                topRight: Radius.circular(15.0),
-              ),
-            ),
-          );
-        },        
-      ),
-    );
-
     // move camera
     _mapController.animateCamera(
       CameraUpdate.newCameraPosition(
@@ -188,10 +261,6 @@ class _MapPageState extends State<MapPage> {
   }
 
   void _setMarker(LatLng coord) async {
-    // get address
-    String _markerAddress = await _getGeolocationAddress(
-      Position(latitude: coord.latitude, longitude: coord.longitude),
-    );
     // add marker
     setState(() {
       _mapMarkers.add(
@@ -201,24 +270,11 @@ class _MapPageState extends State<MapPage> {
           icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
           infoWindow: InfoWindow(
             title: coord.toString(),
-            snippet: _markerAddress,
+            //snippet: _markerAddress,
           ),
           consumeTapEvents: true,
           onTap: () async {
-            await showModalBottomSheet(
-              context: context,
-              builder: (context) => StatefulBuilder(
-                builder: (context, setModalState) =>
-                    _bottomSheet(context,setModalState,coord,_markerAddress),
-              ),
-              isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15.0),
-                  topRight: Radius.circular(15.0),
-                ),
-              ),
-            );
+
           },
         ),
       );
@@ -238,80 +294,118 @@ class _MapPageState extends State<MapPage> {
     return "No address availabe";
   }
 
-  Widget _bottomSheet(BuildContext context,StateSetter setModalState,LatLng coord,String address) {
-    return Padding(
-      padding: EdgeInsets.only(
-        top: 24.0,
-        left: 24,
-        right: 24,
-        bottom: MediaQuery.of(context).viewInsets.bottom,
-      ),
-      child: Container(
-        child: Column(
-          //mainAxisSize: MainAxisSize.min,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 40, horizontal: 70),
-              child: Placeholder(
-                color: Colors.purple,
-                fallbackHeight: 256,
-                //fallbackWidth: 32,
-              ),
-            ),                        
-            Text(
-              "Nombre Apellido",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-              ),
-            ),            
-            SizedBox(
-              height: 10,
-            ),                        
-            Text(
-              address,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              coord.toString(),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 10,
-              )
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(
-                  iconSize: 100,
-                  icon: Icon(Icons.fast_rewind,color:Colors.purple),
-                  onPressed: null
-                ),                   
-                SizedBox(
-                  width: 40,
-                ),                  
-                IconButton(
-                  iconSize: 100,
-                  icon: Icon(Icons.favorite,color:Colors.red),
-                  onPressed: null
-                ),              
-              ],
-            ),                        
-          ],
+  void _showUserCard(BuildContext context, Users user) async {
+
+    String address = await _getGeolocationAddress(
+      Position(latitude: user.coord.latitude, longitude: user.coord.longitude),
+    );
+
+    _mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(
+            user.coord.latitude,
+            user.coord.longitude,
+          ),
+          zoom: 15.0,
         ),
       ),
     );
+
+    await showModalBottomSheet(
+      context: context,
+      builder: (context) => StatefulBuilder(
+        builder: (context, setModalState) {
+          return Padding(
+            padding: EdgeInsets.only(
+              top: 24.0,
+              left: 24,
+              right: 24,
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Container(
+              child: Column(
+                //mainAxisSize: MainAxisSize.min,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 40, horizontal: 120),
+                    child: Placeholder(
+                      color: Colors.purple,
+                      fallbackHeight: 128,
+                      fallbackWidth: 32,
+                    ),
+                  ),                        
+                  Text(
+                    "${user.firstname} " + "${user.lastname}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),            
+                  SizedBox(
+                    height: 10,
+                  ),                        
+                  Text(
+                    address,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        iconSize: 50,
+                        icon: Icon(Icons.favorite,color:Colors.red),
+                        onPressed: null
+                      ),   
+                      SizedBox(
+                        width: 20,
+                      ),
+                      IconButton(
+                        iconSize: 50,
+                        icon: Icon(Icons.format_list_bulleted,color:Colors.purple),
+                        onPressed: null
+                      ),                         
+                      SizedBox(
+                        width: 20,
+                      ),                                                                   
+                      IconButton(
+                        iconSize: 50,
+                        icon: Icon(Icons.fast_forward,color:Colors.purple),
+                        onPressed: (){
+                          Navigator.of(context).pop();
+                          setState((){                            
+                            _user_index++;
+                            if(_user_index == _usersList.length-1){
+                              _user_index = 0;
+                            }
+                          });
+                          _showUserCard(context,_usersList[_user_index]);
+                        }
+                      ),                   
+                    ],
+                  ),                        
+                ],
+              ),
+            ),
+          );
+        }  
+      ),
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          topRight: Radius.circular(15.0),
+        ),
+      ),
+    );    
   } 
 }
 
